d20798903f13c73e5e393b2c8898fc515d79e145
Rima <r>
2018-12-29 13:31:48 +0300

    Initial Commit

---
+++ OnTheMap/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  OnTheMap
+//
+//  Created by admin on 29/12/2018.
+//  Copyright © 2018 Rima. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
